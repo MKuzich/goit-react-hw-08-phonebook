f86cb97909aa6444d27d18e7f8982e8a3becd1a8
Mykhailo Kuzich <timmkedth@gmail.com>
2022-08-26 18:42:59 +0300

    persist saving token for diff sessions added

--- src/index.js
+++ src/index.js
@@ -4,16 +4,19 @@ import { App } from 'components/App';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './theme';
 import { Provider } from 'react-redux';
-import { store } from './redux/store';
+import { store, persistor } from './redux/store';
 import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename="/goit-react-hw-08-phonebook/">
       <ThemeProvider theme={theme}>
         <Provider store={store}>
-          <App />
+          <PersistGate loading={null} persistor={persistor}>
+            <App />
+          </PersistGate>
         </Provider>
       </ThemeProvider>
     </BrowserRouter>
